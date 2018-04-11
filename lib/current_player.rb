655f05a7a7137c6f7bbfa3652f1c3692cdf166bb
Joyce Chun <wooyangee@gmail.com>
2018-04-11 21:26:15 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,5 @@
 def turn_count(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   count = {board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]}
-  count.each do 
+  count.each do |character|
+    
