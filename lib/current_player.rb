d7e4cfe3ea1f30f70dc27f57407efe609794ade7
Joyce Chun <wooyangee@gmail.com>
2018-04-11 21:47:06 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,8 +8,8 @@ def turn_count(board)
  counter
 end
  
-def current_player(turn_count)
-  if #{turn_count(board)} % 2 = 0 
+def current_player(board)
+  if turn_count(board) % 2 = 0 
     return "X"
   else
     return "O"
