323f42666685ef848e1c06d909f48547cdf7bfa6
Joyce Chun <wooyangee@gmail.com>
2018-04-11 19:52:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,11 @@
 def turn_count(board)
+  counter = 0
+  while counter < 10
   player_x = "X"
   player_o = "O" 
   players.each do |character| 
-    puts #{character}
+    puts #{player_x}
+    puts #{player_o}
   counter += 1 
 end
 end
