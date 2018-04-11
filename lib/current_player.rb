73280bba2ac2d51a6dcb14858b4b07791a4243c6
Joyce Chun <wooyangee@gmail.com>
2018-04-11 21:31:30 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,11 @@ def turn_count(board)
  counter = 0 
  board.each do |slot|
    if #{slot} = "X" || #{slot} = "O"
-   puts #{counter}
+   puts "#{counter}"
+   counter += 1 
+   end
+ end
+end
+ 
    
    
